9dc6581cf8cc3980486bbaeb663606ce272d8b57
marina sakai <marina04015@gmail.com>
2016-10-15 15:52:41 +0900

    add: game clear scene

---
+++ Assets/Script/GameClear.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameClear : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnCollisionEnter(Collision hit) {
+        if (hit.gameObject.CompareTag("Player")) {
+            SceneManager.LoadScene("GameClear");
+        }
+    }
+}
